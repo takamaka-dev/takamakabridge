303d54599311e704273345bd9e0c82776fc759eb
isacco.borsani@h2tcoin.com <isacco.borsani@h2tcoin.com>
2021-01-07 18:49:06 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/com/takamaka/test/mavenproject2/AboutusController.java
    	src/main/resources/com/takamaka/test/mavenproject2/main_view.fxml
    	src/main/resources/com/takamaka/test/mavenproject2/primary.fxml

---
+++ src/main/java/com/takamakachain/walletweb/resources/JavaEE8Resource.java
@@ -0,0 +1,46 @@
+package com.takamakachain.walletweb.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+
+    @GET
+    public Response ping() {
+        return Response
+                .ok("ping")
+                .build();
+    }
+
+    @POST
+    @Path("printForm")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response printForm(Wallet wallet) {
+        //process parameters
+        System.out.println(wallet.getEmail());
+        return Response.status(200).build();
+    }
+    
+//    @POST
+//    @Path("printForm")
+//    public void printFormOld() {
+//        System.out.println("ciao mondo");
+//    }
+
+    @GET
+    @Path("testy/{name}")
+    public String ping(@PathParam("name") String name) {
+        return "Hello " + name;
+    }
+
+}
