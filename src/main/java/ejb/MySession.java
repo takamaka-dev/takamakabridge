e0365d320679126758caa40dd0b5a924006cc0a4
isacco.borsani@h2tcoin.com <isacco.borsani@h2tcoin.com>
2021-01-12 18:59:17 +0100

    Merge origin/master
    Conflicts:
    	src/main/webapp/index.html

--- src/main/java/ejb/MySession.java
+++ src/main/java/ejb/MySession.java
@@ -21,74 +21,92 @@ public class MySession implements MySessionLocal {
     private KeyContexts.WalletCypher walletCypher;
     private BigInteger tkgBalance, tkrBalance, frozenTkg, frozenTkr;
 
+    @Override
     public String getWalletAddress() {
         return walletAddress;
     }
 
+    @Override
     public void setWalletAddress(String walletAddress) {
         this.walletAddress = walletAddress;
     }
 
+    @Override
     public String getWalletName() {
         return walletName;
     }
 
+    @Override
     public void setWalletName(String walletName) {
         this.walletName = walletName;
     }
 
+    @Override
     public String getWalletPassword() {
         return walletPassword;
     }
 
+    @Override
     public void setWalletPassword(String walletPassword) {
         this.walletPassword = walletPassword;
     }
 
+    @Override
     public int getWalletIndex() {
         return walletIndex;
     }
 
+    @Override
     public void setWalletIndex(int walletIndex) {
         this.walletIndex = walletIndex;
     }
 
+    @Override
     public KeyContexts.WalletCypher getWalletCypher() {
         return walletCypher;
     }
 
+    @Override
     public void setWalletCypher(KeyContexts.WalletCypher walletCypher) {
         this.walletCypher = walletCypher;
     }
 
+    @Override
     public BigInteger getTkgBalance() {
         return tkgBalance;
     }
 
+    @Override
     public void setTkgBalance(BigInteger tkgBalance) {
         this.tkgBalance = tkgBalance;
     }
 
+    @Override
     public BigInteger getTkrBalance() {
         return tkrBalance;
     }
 
+    @Override
     public void setTkrBalance(BigInteger tkrBalance) {
         this.tkrBalance = tkrBalance;
     }
 
+    @Override
     public BigInteger getFrozenTkg() {
         return frozenTkg;
     }
 
+    @Override
     public void setFrozenTkg(BigInteger frozenTkg) {
         this.frozenTkg = frozenTkg;
     }
 
+    @Override
     public BigInteger getFrozenTkr() {
         return frozenTkr;
     }
 
+    @Override
     public void setFrozenTkr(BigInteger frozenTkr) {
         this.frozenTkr = frozenTkr;
     }
