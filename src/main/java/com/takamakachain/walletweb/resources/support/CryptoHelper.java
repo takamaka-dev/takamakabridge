80ec79502b11de7cecbb551617553fe9301ce033
giovanni.antino@h2tcoin.com <giovanni.antino@h2tcoin.com@poldo.h2tcoin.com>
2021-01-19 12:38:37 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/com/takamakachain/walletweb/resources/JavaEE8Resource.java

--- src/main/java/com/takamakachain/walletweb/resources/support/CryptoHelper.java
+++ src/main/java/com/takamakachain/walletweb/resources/support/CryptoHelper.java
@@ -8,8 +8,13 @@ package com.takamakachain.walletweb.resources.support;
 import com.h2tcoin.takamakachain.saturn.SatUtils;
 import com.h2tcoin.takamakachain.saturn.exceptions.SaturnException;
 import com.h2tcoin.takamakachain.utils.FileHelper;
+import com.h2tcoin.takamakachain.utils.threadSafeUtils.TkmSignUtils;
+import com.h2tcoin.takamakachain.utils.threadSafeUtils.TkmTextUtils;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import org.apache.commons.lang3.RandomStringUtils;
 
@@ -28,7 +33,20 @@ public class CryptoHelper {
         new SecureRandom().nextBytes(iv);
         return new IvParameterSpec(iv);
     }
-    
-    //public static final SecretKey getNewAesSecretKey()
+
+    public static final SecretKey getNewAesSecretKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        SecretKey sk = keyGenerator.generateKey();
+        return sk;
+    }
+
+    public static final String ivToHex(IvParameterSpec iv) {
+        return TkmSignUtils.fromByteArrayToHexString(iv.getIV());
+    }
+
+    public static final IvParameterSpec hexToIv(String hex) {
+        return new IvParameterSpec(TkmSignUtils.fromHexToByteArray(hex));
+    }
 
 }
